3f84d10f963979c389aa10fdb82001f9ccb31eee
Dimitris Asimakopoulos <sysadmin@droilogic.com>
2023-10-24 21:17:31 +0300

    update user

--- src/app/appsrv.service.ts
+++ src/app/appsrv.service.ts
@@ -16,11 +16,17 @@ export class AppsrvService {
   }
 
   getUserById(userId: number) {
-    return this.http.get<Person>(this.endpoint + `/users/${userId}`)
+    return this.http.get<Person>(`${this.endpoint}/users/${userId}`)
   }
 
   addUser(user: Person) {
     return this.http.post<Person>(this.endpoint + '/users', user);
+  }
+
+  updateUser(user: Person) {
+    console.log('SERVICE: ', user);
+    
+    return this.http.put<Person>(`${this.endpoint}/users/${user.id}`, user);
   }
 
   deleteUser(userId: number) {
--- src/app/crud-demo/crud/update-user/update-user.component.ts
+++ src/app/crud-demo/crud/update-user/update-user.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CrudUserSearchComponent } from '../../utils/crud-user-search/crud-user-search.component';
+import { Person } from 'src/app/interfaces/person';
+import { CrudUserFormComponent } from '../../utils/crud-user-form/crud-user-form.component';
+import { AppsrvService } from 'src/app/appsrv.service';
 
 @Component({
   selector: 'app-update-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CrudUserSearchComponent, CrudUserFormComponent],
   templateUrl: './update-user.component.html',
   styleUrls: ['./update-user.component.css']
 })
 export class UpdateUserComponent {
+  @Output() userUpdated = new EventEmitter();
+
+  constructor(private service: AppsrvService = inject(AppsrvService)) {}
+
+  foundUser: Person | undefined;
+
+  onUserFound(user: Person | undefined) {
+    if(user) {
+      this.foundUser = user;
+    }
+  }
+
+  onUpdate(user: Person) {
+    this.service.updateUser(user).subscribe((user) => {
+      console.log(user);
+      this.userUpdated.emit();
+    });
+  }
 
 }
